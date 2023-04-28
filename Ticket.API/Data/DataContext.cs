4b16835367a996d7ac0d65380c02971f4c852796
Jhon Munoz <jhonmunoz225205@correo.itm.edu.co>
2023-04-28 18:41:17 -0500

    Structure base

---
+++ Ticket.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+    
+namespace Ticket.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ Ticket.Shared/Entities/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ticket.Shared.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Fecha")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Usada")]
+        public bool Used { get; set; } = false;
+
+        [Display(Name = "Porteria")]
+        public string Zone { get; set; } = null!;
+    }
+}
